4a6b8cf0ce89ecc4c4bf4141ee31138fe73a0eff
Giorgio Torres <torres.giorgio@gmail.com>
2018-02-25 20:12:27 -0300

    FactoryBot installation and configured for model Unidade

--- test/models/unidade_test.rb
+++ test/models/unidade_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class UnidadeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the model's attributes built by factory should be valid" do
+    unidade = build(:unidade)
+
+    assert unidade.nome.present?
+    assert unidade.lat.present?
+    assert unidade.long.present?
+    assert unidade.totalCarros.present?
+    assert unidade.valid?
+    assert unidade.save
+  end
 end
